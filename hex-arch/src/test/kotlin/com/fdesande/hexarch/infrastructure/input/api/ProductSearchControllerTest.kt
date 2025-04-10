699471b78927534c4d99700254d09287ff4fbee6
Fer de Sande <fernando.desande@bitingbit.com>
2025-04-10 23:43:44 +0100

    feat(test): Add database integration tests for ProductSearchController

--- hex-arch/src/test/kotlin/com/fdesande/hexarch/infrastructure/input/api/ProductSearchControllerTest.kt
+++ hex-arch/src/test/kotlin/com/fdesande/hexarch/infrastructure/input/api/ProductSearchControllerTest.kt
@@ -1,12 +1,16 @@
 package com.fdesande.hexarch.infrastructure.input.api
 
-import com.fdesande.hexarch.domain.model.*
-import com.fdesande.hexarch.domain.port.input.ProductSearchUseCase
+import com.fdesande.hexarch.domain.model.Image
+import com.fdesande.hexarch.domain.model.Price
+import com.fdesande.hexarch.domain.model.Product
+import com.fdesande.hexarch.domain.model.ProductId
 import com.fdesande.hexarch.infrastructure.config.TestcontainersConfiguration
 import com.fdesande.hexarch.infrastructure.input.api.dto.ImageDto
 import com.fdesande.hexarch.infrastructure.input.api.dto.PriceDto
 import com.fdesande.hexarch.infrastructure.input.api.dto.ProductDto
 import com.fdesande.hexarch.infrastructure.input.api.exception.ErrorResponse
+import com.github.database.rider.core.api.dataset.DataSet
+import com.github.database.rider.junit5.api.DBRider
 import io.restassured.RestAssured
 import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Given
@@ -16,10 +20,9 @@ import org.apache.http.HttpStatus
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.BeforeEach
-import org.mockito.kotlin.reset
-import org.mockito.kotlin.whenever
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -30,32 +33,81 @@ import kotlin.test.Test
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = [TestcontainersConfiguration.Initializer::class])
+@DBRider
+@DataSet("query-products/fixtures.json")
 class ProductSearchControllerTest {
 
-    private val image1 = Image(UUID.randomUUID(), "https://some.url", "first image")
-    private val image2 = Image(UUID.randomUUID(), "https://another.url", "first image")
+    companion object {
+        private const val FILTER_PARAM = "filter"
+        private const val MIN_PRICE_PARAM = "minPrice"
+        private const val MAX_PRICE_PARAM = "maxPrice"
+    }
 
-    @MockBean
-    private lateinit var productSearchUseCase: ProductSearchUseCase
+    private val product1 = Product(
+        id = ProductId(UUID.fromString("00000000-0000-0000-0000-000000000001")),
+        description = "A fancy hoodie with a blue camouflage",
+        name = "Blue camouflage hoodie",
+        brand = "Nike",
+        price = Price(BigDecimal.valueOf(37.21)),
+        imageUrls = listOf(
+            Image(
+                id = UUID.fromString("00000000-0000-0000-0000-000000000000"),
+                url = "https://img/nike/hoodie-front.png",
+                name = "Front"
+            ),
+            Image(
+                id = UUID.fromString("00000000-0000-0001-0001-000000000002"),
+                url = "https://img/nike/hoodie-front-with-model.png",
+                name = "Front with model"
+            )
+        )
+    )
+    private val product2 = Product(
+        id = ProductId(UUID.fromString("00000000-0000-0000-0000-000000000002")),
+        description = "Top performance football shoes",
+        name = "Adidas predator 2025",
+        brand = "Adidas",
+        price = Price(BigDecimal.valueOf(125.99)),
+        imageUrls = listOf(
+            Image(
+                id = UUID.fromString("00000000-0000-0001-0002-000000000001"),
+                name = "Red Predator",
+                url = "https://img/adidas/predator-25-red.png",
+            ),
+            Image(
+                id = UUID.fromString("00000000-0000-0001-0002-000000000002"),
+                name = "Blue Predator",
+                url = "https://img/adidas/predator-25-blue.png",
+            ),
+            Image(
+                id = UUID.fromString("00000000-0000-0001-0002-000000000003"),
+                name = "Pink Predator",
+                url = "https://img/adidas/predator-25-pink.png",
+            )
+        )
+    )
+    private val product3 = Product(
+        id = ProductId(UUID.fromString("00000000-0000-0000-0000-000000000003")),
+        description = "The Argentinian official football T-Shirt 2025",
+        name = "Argentina football T-Shirt 2025",
+        brand = "Adidas",
+        price = Price(BigDecimal.valueOf(90.00)),
+        imageUrls = emptyList()
+    )
 
     @LocalServerPort
     private val localPort = 0
 
     @BeforeEach
     fun setUp() {
-        reset(productSearchUseCase)
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
         RestAssured.baseURI = "http://localhost:$localPort/products"
     }
 
     @Test
     fun `get products return dtos when data is found`() {
-        // Given
-        val query = ProductQuery()
-        val products = (0..4).map { createProduct(it) }.toSet()
-        whenever(productSearchUseCase.search(query)).thenReturn(products)
-
-        val dtos = Given {
+        val expectedProducts = listOf(product1, product2, product3).map(::createProductDto)
+        val actualProducts = Given {
             header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
         } When {
             get()
@@ -66,18 +118,111 @@ class ProductSearchControllerTest {
         }
 
         // Then
-        assertThat(dtos, hasSize(products.size))
-        assertThat(dtos, equalTo(products.map(::createProductDto)))
+        assertThat(actualProducts, equalTo(expectedProducts))
     }
 
     @Test
-    fun `get products return an empty list when no product is found`() {
-        // Given
-        val query = ProductQuery()
-        whenever(productSearchUseCase.search(query)).thenReturn(emptySet())
+    fun `get products return filtered result by name`() {
+        val expectedProducts = listOf(product1).map(::createProductDto)
+        val actualProducts = Given {
+            header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            queryParams(FILTER_PARAM, "CAMOUFLAGE")
+        } When {
+            get()
+        } Then {
+            statusCode(HttpStatus.SC_OK)
+        } Extract {
+            body().jsonPath().getList("", ProductDto::class.java)
+        }
 
+        // Then
+        assertThat(actualProducts, equalTo(expectedProducts))
+    }
+
+    @Test
+    fun `get products return filtered result by description`() {
+        val expectedProducts = listOf(product2, product3).map(::createProductDto)
+        val actualProducts = Given {
+            header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            queryParams(FILTER_PARAM, "foot")
+        } When {
+            get()
+        } Then {
+            statusCode(HttpStatus.SC_OK)
+        } Extract {
+            body().jsonPath().getList("", ProductDto::class.java)
+        }
+
+        // Then
+        assertThat(actualProducts, equalTo(expectedProducts))
+    }
+
+    @Test
+    fun `get products return filtered result by brand`() {
+        val expectedProducts = listOf(product2, product3).map(::createProductDto)
+        val actualProducts = Given {
+            header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            queryParams(FILTER_PARAM, "adidas")
+        } When {
+            get()
+        } Then {
+            statusCode(HttpStatus.SC_OK)
+        } Extract {
+            body().jsonPath().getList("", ProductDto::class.java)
+        }
+
+        // Then
+        assertThat(actualProducts, equalTo(expectedProducts))
+    }
+
+    @Test
+    fun `get products return filtered result by min price`() {
+        val expectedProducts = listOf(product2, product3).map(::createProductDto)
+        val actualProducts = Given {
+            header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            queryParams(MIN_PRICE_PARAM, "45.00")
+        } When {
+            get()
+        } Then {
+            statusCode(HttpStatus.SC_OK)
+        } Extract {
+            body().jsonPath().getList("", ProductDto::class.java)
+        }
+
+        // Then
+        assertThat(actualProducts, equalTo(expectedProducts))
+    }
+
+    @Test
+    fun `get products return filtered result by max price`() {
+        val expectedProducts = listOf(product1, product3).map(::createProductDto)
+        val actualProducts = Given {
+            header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            queryParams(MAX_PRICE_PARAM, "100.00")
+        } When {
+            get()
+        } Then {
+            statusCode(HttpStatus.SC_OK)
+        } Extract {
+            body().jsonPath().getList("", ProductDto::class.java)
+        }
+
+        // Then
+        assertThat(actualProducts, equalTo(expectedProducts))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "$FILTER_PARAM, Brooks",
+            "$MIN_PRICE_PARAM, 1342.12",
+            "$MAX_PRICE_PARAM, 1.99",
+        ]
+    )
+    fun `get products return an empty list when no product is found`(parameter: String, value: String) {
         Given {
             header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+            queryParams(parameter, value)
         } When {
             get()
         } Then {
@@ -88,13 +233,10 @@ class ProductSearchControllerTest {
 
     @Test
     fun `get product by id return dto when data is found`() {
-        // Given
-        val product = createProduct()
-        whenever(productSearchUseCase.searchById(product.id)).thenReturn(product)
-
+        val productUUID = product1.id.id
         val dto = Given {
             header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-            pathParam("id", product.id.id)
+            pathParam("id", productUUID)
         } When {
             get("/{id}")
         } Then {
@@ -105,41 +247,29 @@ class ProductSearchControllerTest {
 
         // Then
         assertThat(dto, notNullValue())
-        assertThat(dto, equalTo(createProductDto(product)))
+        assertThat(dto, equalTo(createProductDto(product1)))
     }
 
     @Test
     fun `get product by id return not found when product does not exist`() {
         // Given
-        val product = createProduct()
-        whenever(productSearchUseCase.searchById(product.id)).thenReturn(null)
+        val productUUID = "99999999-9999-9999-9999-999999999999"
 
         Given {
             header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-            pathParam("id", product.id.id)
+            pathParam("id", productUUID)
         } When {
             get("/{id}")
         } Then {
             statusCode(HttpStatus.SC_NOT_FOUND)
             body("timestamp", notNullValue())
-            body("message", equalTo("Product with id: ${product.id.id} not found"))
+            body("message", equalTo("Product with id: $productUUID not found"))
             body("status", equalTo(HttpStatus.SC_NOT_FOUND))
             body("error", equalTo("NOT_FOUND"))
         } Extract {
             body().jsonPath().getObject("", ErrorResponse::class.java)
         }
     }
-
-    private fun createProduct(
-        productNumber: Int = 1
-    ) = Product(
-        id = ProductId(UUID.randomUUID()),
-        name = "product $productNumber",
-        description = "sample description",
-        brand = "some brand",
-        price = Price(BigDecimal.valueOf(productNumber * 10.00)),
-        imageUrls = listOf(image1, image2),
-    )
 
     private fun createProductDto(
         product: Product
